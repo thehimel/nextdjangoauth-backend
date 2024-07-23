db21814ef97a696e23e9ea3930de2a6751cb4da1
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-07-23 22:11:26 +0200

    integrated django-extensions

--- src/core/settings.py
+++ src/core/settings.py
@@ -63,6 +63,8 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    # --- Utilities
+    'django_extensions',  # Generate ER Diagram.
     # --- Storage
     'cloudinary_storage',
     'cloudinary',
