49fb7e320dc9498cc796ebc6160452d2b7928934
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-07-24 01:09:40 +0200

    updated url patterns for authentication

--- src/core/urls.py
+++ src/core/urls.py
@@ -27,8 +27,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     re_path('auth/registration/account-confirm-email/(?P<key>.+)/', confirm_email, name='account_confirm_email'),
-    path('auth/', include('dj_rest_auth.urls')),
-    path('auth/registration/', include('dj_rest_auth.registration.urls')),
+    path('api/auth/', include('dj_rest_auth.urls')),
+    path('api/auth/registration/', include('dj_rest_auth.registration.urls')),
 ]
 
 oas_urls = [
