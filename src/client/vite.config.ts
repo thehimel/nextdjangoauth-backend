cbc81b5cc7c07ea047461c2e1db344472c367741
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-07-24 19:06:44 +0200

    configure vite.config.ts

--- src/client/vite.config.ts
+++ src/client/vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const API_URL = process.env.API_URL || 'http://127.0.0.1:8000';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    assetsDir: 'static'
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: API_URL,
+        changeOrigin: true,
+      }
+    }
+  },
 })
