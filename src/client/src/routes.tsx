131ee0739725c0acd9e47420a23947383e042e75
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-07-25 21:07:10 +0200

    added signup route in client

--- src/client/src/routes.tsx
+++ src/client/src/routes.tsx
@@ -1,4 +1,5 @@
 import App from "@/App.tsx";
+import Signup from "@/components/Auth/Signup/Signup.tsx";
 import Home from "@/Home.tsx";
 import {createBrowserRouter, Route, createRoutesFromElements} from "react-router-dom";
 
@@ -6,6 +7,7 @@ export const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App/>}>
       <Route index={true} path="/" element={<Home/>}/>
+      <Route path="/signup" element={<Signup/>}/>
     </Route>
   )
 )
