c2ae83339bdb3733592788988cbc3e1ad7e5bebf
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-07-25 02:24:26 +0200

    added configuration for vercel

---
+++ src/build.sh
@@ -0,0 +1,15 @@
+echo "Building react application..."
+cd client && npm install --force && npx vite build && rm -rf node_modules && cd ..
+
+echo "Upgrading pip..."
+python3 -m pip install --upgrade pip
+
+echo "Installing packages..."
+pip install -r requirements.txt
+
+echo "Migrating Database..."
+python3 manage.py makemigrations
+python3 manage.py migrate
+
+echo "Collecting static files."
+python3 manage.py collectstatic --no-input
