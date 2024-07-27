f50fe924ef9945d1b92527afc43c9d2ffde8df8d
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-07-27 19:46:56 +0200

    server: reformated code

--- src/apps/base/urls.py
+++ src/apps/base/urls.py
@@ -2,10 +2,10 @@ from django.urls import path, re_path
 
 from apps.base.views.client import ClientView
 
-app_name = "base"
+app_name = 'base'
 
 urlpatterns = [
-    path("", ClientView.as_view(), name="client"),
-    path("signup/", ClientView.as_view(), name="signup"),
+    path('', ClientView.as_view(), name='client'),
+    path('signup/', ClientView.as_view(), name='signup'),
     re_path('signup/confirm-email/(?P<key>.+)/', ClientView.as_view(), name='confirm_email'),
 ]
