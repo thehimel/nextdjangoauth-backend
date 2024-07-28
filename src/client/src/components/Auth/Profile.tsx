88a2365d98f8c35bbede3a47e4a4d2a92effbbfa
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-07-28 07:58:16 +0200

    client: update Profile.tsx

--- src/client/src/components/Auth/Profile.tsx
+++ src/client/src/components/Auth/Profile.tsx
@@ -1,11 +1,26 @@
 "use client";
 
+import {useAppSelector} from "@/store/hooks.ts";
 import type {CardProps} from "@nextui-org/react";
 
 import {Card, CardHeader, CardBody, Button, Avatar, Badge, Input, CardFooter} from "@nextui-org/react";
+import {useEffect} from "react";
+import {useLocation, useNavigate} from "react-router-dom";
 
 const Profile = (props: CardProps) => {
-  return (
+  const navigate = useNavigate();
+  const location = useLocation();
+  const isLoggedIn = useAppSelector((state) => state.auth.loggedIn);
+
+  useEffect(() => {
+    if (!isLoggedIn) {
+      // Store the path to redirect to after login
+      const redirectPath = location.pathname;
+      navigate('/login', { state: { from: redirectPath } });
+    }
+  }, [isLoggedIn, navigate, location]);
+
+  return isLoggedIn ? (
     <Card className="max-w-xl p-2" {...props}>
       <CardHeader className="flex flex-col items-start px-4 pb-0 pt-4">
         <p className="text-large text-center">Account Details</p>
@@ -31,7 +46,7 @@ const Profile = (props: CardProps) => {
         </Button>
       </CardFooter>
     </Card>
-  );
+  ) : null;
 }
 
 export default Profile;
