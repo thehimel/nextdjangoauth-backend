238799359a8d4229d458a9d5831cd8aa278059b2
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-07-28 21:48:24 +0200

    client: init ChangePassword.tsx

---
+++ src/client/src/components/user/ChangePassword.tsx
@@ -0,0 +1,134 @@
+"use client";
+
+import {EyeClosedIcon, EyeOpenIcon} from "@/components/icons/eyes.tsx";
+import ProfileHeader from "@/components/user/ProfileHeader.tsx";
+import ProfileFooter from "@/components/user/ProfileFooter.tsx";
+import {LOGIN_URL, PROFILE_URL} from "@/components/utils/constants.ts";
+import {useAppDispatch, useAppSelector} from "@/store/hooks.ts";
+import {AppDispatch} from "@/store/store.ts";
+import {isValidPassword, validateField} from "@/utils/validate.ts";
+import {CardProps} from "@nextui-org/react";
+
+import {Card, CardBody, Input} from "@nextui-org/react";
+import React, {FormEvent, useEffect} from "react";
+import {useLocation, useNavigate} from "react-router-dom";
+
+const ChangePassword = (props: CardProps) => {
+  const dispatch: AppDispatch = useAppDispatch();
+
+  const navigate = useNavigate();
+  const location = useLocation();
+  const redirectPath = location.pathname;  // Store the path to redirect to after login
+
+  const isLoggedIn = useAppSelector((state) => state.auth.loggedIn);
+  const [isLoading, setIsLoading] = React.useState(false);
+
+  const access = useAppSelector((state) => state.auth.userData.access);
+  const email = useAppSelector((state) => state.auth.userData.user.email);
+  const firstName = useAppSelector((state) => state.auth.userData.user.first_name);
+  const lastName = useAppSelector((state) => state.auth.userData.user.last_name);
+
+  const [confirmPassword, setConfirmPassword] = React.useState("");
+  const [confirmPasswordErrorMessage, setConfirmPasswordErrorMessage] = React.useState("");
+  const [isConfirmPasswordValid, setIsConfirmPasswordValid] = React.useState(true);
+  const [isConfirmPasswordVisible, setIsConfirmPasswordVisible] = React.useState(false);
+  const toggleConfirmPasswordVisibility = () => setIsConfirmPasswordVisible(!isConfirmPasswordVisible);
+
+  const [password, setPassword] = React.useState("");
+  const [passwordErrorMessage, setPasswordErrorMessage] = React.useState("");
+  const [isPasswordValid, setIsPasswordValid] = React.useState(true);
+  const [isPasswordVisible, setIsPasswordVisible] = React.useState(false);
+  const togglePasswordVisibility = () => setIsPasswordVisible(!isPasswordVisible);
+
+  useEffect(() => {
+    if (!isLoggedIn) {
+      navigate(LOGIN_URL, { state: { from: redirectPath } });
+    }
+  }, [isLoggedIn, navigate, redirectPath]);
+
+  const handleSubmit = async (event: FormEvent) => {
+    event.preventDefault();
+    let isFormValid = true;
+
+    isFormValid = validateField({
+      isValid: isValidPassword(password),
+      setIsFieldValid: setIsPasswordValid,
+      setFieldErrorMessage: setPasswordErrorMessage,
+      errorMessage: "Enter a valid password.",
+    }) && isFormValid;
+
+    isFormValid = validateField({
+      isValid: isValidPassword(confirmPassword) && confirmPassword === password,
+      setIsFieldValid: setIsConfirmPasswordValid,
+      setFieldErrorMessage: setConfirmPasswordErrorMessage,
+      errorMessage: confirmPassword !== password ? "Passwords do not match." : "Enter a valid password."
+    }) && isFormValid;
+
+    if (isFormValid) {
+      setIsLoading(true);
+      console.log(true);
+      setIsLoading(false);
+    }
+  };
+
+  return isLoggedIn ? (
+      <Card className="max-w-xl p-2" {...props}>
+        <ProfileHeader
+          title={"Account Details"}
+          firstName={firstName}
+          lastName={lastName}
+          email={email}
+          navigationLink={{url: PROFILE_URL, title: "Update Profile"}}
+        />
+        <form onSubmit={handleSubmit}>
+          <CardBody className="grid grid-cols-1 gap-4 md:grid-cols-2">
+            <Input
+              isRequired
+              endContent={
+                <button type="button" onClick={togglePasswordVisibility}>
+                  {isPasswordVisible ? EyeClosedIcon : EyeOpenIcon}
+                </button>
+              }
+              label="Password"
+              name="password"
+              autoComplete="new-password"
+              variant="bordered"
+              type={isPasswordVisible ? "text" : "password"}
+              errorMessage={!isPasswordValid ? passwordErrorMessage : undefined}
+              isInvalid={!isPasswordValid}
+              isDisabled={isLoading}
+              value={password}
+              onValueChange={(value) => {
+                setIsPasswordValid(true);
+                setPassword(value);
+              }}
+            />
+            <Input
+                isRequired
+                endContent={
+                  <button type="button" onClick={toggleConfirmPasswordVisibility}>
+                    {isConfirmPasswordVisible ? EyeClosedIcon : EyeOpenIcon}
+                  </button>
+                }
+                label="Confirm Password"
+                name="confirmPassword"
+                autoComplete="new-password"
+                variant="bordered"
+                type={isConfirmPasswordVisible ? "text" : "password"}
+                errorMessage={!isConfirmPasswordValid ? confirmPasswordErrorMessage : undefined}
+                isInvalid={!isConfirmPasswordValid}
+                isDisabled={isLoading}
+                value={confirmPassword}
+                onValueChange={(value) => {
+                  setIsConfirmPasswordValid(true);
+                  setConfirmPassword(value);
+                }}
+              />
+          </CardBody>
+          <ProfileFooter title={"Save"} isLoading={isLoading} />
+        </form>
+    </Card>
+  ) : null;
+}
+
+export default ChangePassword;
