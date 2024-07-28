6284575979efee2daa285f43dd9d8790a8942ecd
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-07-28 21:48:14 +0200

    client: routes.tsx: take urls from file

--- src/client/src/routes.tsx
+++ src/client/src/routes.tsx
@@ -1,18 +1,28 @@
 import App from "@/App.tsx";
 import Auth from "@/components/auth/Auth.tsx";
 import ConfirmEmail from "@/components/auth/ConfirmEmail.tsx";
+import ChangePassword from "@/components/user/ChangePassword.tsx";
 import Profile from "@/components/user/Profile.tsx";
+import {
+  CHANGE_PASSWORD_URL,
+  CONFIRM_EMAIL_URL,
+  HOME_URL,
+  LOGIN_URL,
+  PROFILE_URL,
+  SIGNUP_URL,
+} from "@/components/utils/constants.ts";
 import Home from "@/Home.tsx";
 import {createBrowserRouter, Route, createRoutesFromElements} from "react-router-dom";
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<App/>}>
-      <Route index={true} path="/" element={<Home/>}/>
-      <Route path="/signup/" element={<Auth pageType="signup" headline="Create your account to get started." />}/>
-      <Route path="/login/" element={<Auth pageType="login" headline="Log in to your account to continue." />}/>
-      <Route path="/signup/confirm-email/:key/" element={<ConfirmEmail />}/>
-      <Route path="/profile/" element={<Profile />}/>
+    <Route path={HOME_URL} element={<App/>}>
+      <Route index={true} path={HOME_URL} element={<Home/>}/>
+      <Route path={SIGNUP_URL} element={<Auth pageType="signup" headline="Create your account to get started." />}/>
+      <Route path={LOGIN_URL} element={<Auth pageType="login" headline="Log in to your account to continue." />}/>
+      <Route path={CONFIRM_EMAIL_URL} element={<ConfirmEmail />}/>
+      <Route path={PROFILE_URL} element={<Profile />}/>
+      <Route path={CHANGE_PASSWORD_URL} element={<ChangePassword />}/>
     </Route>
   )
 )
