7cb842126564b9d7e39a32767dc15f21a8aaa618
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-07-30 02:48:05 +0200

    server: reconfigured reset password confirmation url

--- src/apps/base/urls.py
+++ src/apps/base/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('login/', ClientView.as_view(), name='login'),
     re_path('signup/confirm-email/(?P<key>.+)/', ClientView.as_view(), name='confirm_email'),
     re_path('recovery/forgot-password/', ClientView.as_view(), name='forgot_password'),
+    re_path('recovery/reset-password/', ClientView.as_view(), name='reset_password'),
     re_path('profile/', ClientView.as_view(), name='profile'),
     re_path('profile/change-password', ClientView.as_view(), name='change_password'),
 ]
