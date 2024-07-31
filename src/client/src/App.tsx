40dc45a34ff9e17a58151f8afec66c17a7707129
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-07-31 21:43:13 +0200

    client: added navbar in App.tsx

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -1,10 +1,10 @@
+import NavBar from "@/components/navigation/navbar/NavBar.tsx";
 import {useTheme} from "next-themes";
 import {useEffect} from "react";
 import {Outlet} from "react-router-dom";
 import {Toaster} from "sonner";
 import {useAppSelector} from "./store/hooks.ts";
 import Footer from "@/components/navigation/Footer.tsx";
-import NavigationBar from "@/components/navigation/NavigationBar.tsx";
 
 const App = () => {
   const darkMode = useAppSelector((state) => state.base.darkMode);
@@ -15,7 +15,7 @@ const App = () => {
 
   return (
     <div className="min-h-screen">
-      <NavigationBar/>
+      <NavBar/>
       <Toaster position="top-center" theme={darkMode ? "dark" : "light"} />
       <main className="mb-14 p-8 flex items-start justify-center">
         <Outlet/>
