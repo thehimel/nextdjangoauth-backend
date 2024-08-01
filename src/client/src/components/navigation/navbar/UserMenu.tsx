79f162fc38c32f94afd66c141c321ed1697402f3
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-08-01 02:50:16 +0200

    client: navbar: update user menu

--- src/client/src/components/navigation/navbar/UserMenu.tsx
+++ src/client/src/components/navigation/navbar/UserMenu.tsx
@@ -2,7 +2,7 @@ import {PROFILE_URL} from "@/constants/urls.ts";
 import {useAppSelector} from "@/store/hooks.ts";
 import {
   Avatar,
-  Badge,
+  Button,
   Dropdown,
   DropdownItem,
   DropdownMenu, DropdownSection,
@@ -14,35 +14,26 @@ import {
 const UserMenu = () => {
   const userData = useAppSelector((state) => state.auth.userData);
   const fullName = `${userData.user.first_name} ${userData.user.last_name}`
+  const avatarTitle = fullName.length > 1 ? fullName : userData.user.username
+  const avatarImage = "/static/avatar.svg"
 
   return (
     <NavbarItem className="px-2">
       <Dropdown placement="bottom-end">
         <DropdownTrigger>
-          <button className="mt-1 h-8 w-8 outline-none transition-transform">
-            <Badge color="success" content="" placement="bottom-right" shape="circle">
-              <Avatar size="sm" src="/static/avatar.svg" />
-            </Badge>
-          </button>
+          <Button disableRipple isIconOnly className="-mr-1" radius="full" variant="light">
+            <Avatar className="h-6 w-6 cursor-pointer" name={avatarTitle} src={avatarImage}/>
+          </Button>
         </DropdownTrigger>
         <DropdownMenu aria-label="Profile Actions" variant="flat">
           <DropdownSection showDivider aria-label="profile">
             <DropdownItem key="profile" textValue="profile" className="h-12 gap-2">
               <Link href={PROFILE_URL} color="foreground">
                 <User
-                  avatarProps={{
-                    size: "sm",
-                    imgProps: {
-                      className: "transition-none",
-                    },
-                    src: "/static/avatar.svg",
-                  }}
-                  classNames={{
-                    name: "font-semibold",
-                    description: "text-default-500",
-                  }}
-                  name={fullName.length > 1 ? fullName : userData.user.username}
-                  description={userData.user.email}
+                  avatarProps={{size: "sm", imgProps: {className: "transition-none"}, src: avatarImage}}
+                  classNames={{name: "font-semibold", description: "text-default-500"}}
+                  name={avatarTitle}
+                  description="General User"
                 />
               </Link>
 
@@ -58,9 +49,7 @@ const UserMenu = () => {
             <DropdownItem key="feedback" textValue="Feedback">Feedback</DropdownItem>
           </DropdownSection >
 
-          <DropdownSection aria-label="Logout">
-            <DropdownItem key="logout" textValue="Log Out" color="danger">Log Out</DropdownItem>
-          </DropdownSection>
+          <DropdownItem key="logout" textValue="Log Out" color="danger">Log Out</DropdownItem>
         </DropdownMenu>
       </Dropdown>
     </NavbarItem>
