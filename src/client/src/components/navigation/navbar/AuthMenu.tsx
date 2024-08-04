22879695e594227022310806c236f23ab95050f3
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-08-05 01:16:10 +0200

    client: use translations in AuthMenu.tsx

--- src/client/src/components/navigation/navbar/AuthMenu.tsx
+++ src/client/src/components/navigation/navbar/AuthMenu.tsx
@@ -1,21 +1,23 @@
 import {LOGIN_URL, SIGNUP_URL} from "@/constants/urls.ts";
 import {Button, Link, NavbarItem} from "@nextui-org/react";
+import {useTranslation} from "react-i18next";
 
 const UserMenu = () => {
+  const { t } = useTranslation();
 
   return (
     <div className="flex ml-2 gap-2">
       <NavbarItem className="sm:flex">
         <Link href={LOGIN_URL}>
           <Button radius="full" variant="bordered" color="primary" >
-          <p>Login</p>
+          <p>{t("navigation.login")}</p>
         </Button>
         </Link>
       </NavbarItem>
       <NavbarItem className="sm:flex">
         <Link href={SIGNUP_URL}>
           <Button radius="full" variant="shadow" color="primary" >
-          <p>Sign Up</p>
+          <p>{t("navigation.signup")}</p>
         </Button>
         </Link>
       </NavbarItem>
