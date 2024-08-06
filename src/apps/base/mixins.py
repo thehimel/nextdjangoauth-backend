6498d1d78ae391d6988f8fe10898b3d7a5018662
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-08-07 01:03:47 +0200

    server: check if email is registered during social login

--- src/apps/base/mixins.py
+++ src/apps/base/mixins.py
@@ -1,5 +1,6 @@
 from django.utils.translation import gettext_lazy as _
 from rest_framework.response import Response
+from django.contrib.auth.models import User
 from rest_framework import status
 from allauth.socialaccount.models import SocialAccount
 
@@ -18,6 +19,23 @@ class SocialAccountCheckMixin:
                         'provider': provider,
                         'email': [_(f'This email is already registered with {provider} sign-in.'
                                     f' Please use {provider} to log in or use a different email.')]
+                    },
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+        return None
+
+
+class EmailCheckMixin:
+    @staticmethod
+    def check_and_respond_if_email_registered(request):
+        email = request.data.get('email')
+        if email:
+            if User.objects.filter(email=email).exists():
+                return Response(
+                    {
+                        'code': 'email_registered_with_email_login',
+                        'email': [_(f'This email is already registered with an email and password.'
+                                    f' Please use your email and password to log in or use a different email.')]
                     },
                     status=status.HTTP_400_BAD_REQUEST
                 )
--- src/apps/base/views/google_login.py
+++ src/apps/base/views/google_login.py
@@ -1,6 +1,17 @@
 from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
 from dj_rest_auth.registration.views import SocialLoginView
 
+from apps.base.mixins import EmailCheckMixin
 
-class GoogleLogin(SocialLoginView):
+
+class GoogleLogin(EmailCheckMixin, SocialLoginView):
     adapter_class = GoogleOAuth2Adapter
+
+    def post(self, request, *args, **kwargs):
+        # Check if the email is registered with a traditional email and password before proceeding
+        response = self.check_and_respond_if_email_registered(request)
+        if response:
+            return response
+
+        # Proceed with the normal flow if the email is not registered with a traditional account
+        return super().post(request, *args, **kwargs)
