8d5dd954953b3608a36f3be2993d25dd2bb80ccd
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-08-15 11:36:16 +0200

    server: updated auth/urls.py

--- src/apps/auth/urls.py
+++ src/apps/auth/urls.py
@@ -6,15 +6,17 @@ from apps.auth.views.google_login import GoogleLogin
 app_name = 'auth'
 
 urlpatterns = [
+    # Client
     path('', ClientView.as_view(), name='client'),
     path('signup/', ClientView.as_view(), name='signup'),
     path('login/', ClientView.as_view(), name='login'),
     path('loader/', ClientView.as_view(), name='loader'),
     re_path('signup/confirm-email/(?P<key>.+)/', ClientView.as_view(), name='confirm_email'),
-    re_path('recovery/forgot-password/', ClientView.as_view(), name='forgot_password'),
+    path('recovery/forgot-password/', ClientView.as_view(), name='forgot_password'),
     re_path('recovery/reset-password/', ClientView.as_view(), name='reset_password'),
-    re_path('profile/', ClientView.as_view(), name='profile'),
-    re_path('profile/change-password', ClientView.as_view(), name='change_password'),
-    path('api/auth/google/', GoogleLogin.as_view(), name='google_auth'),
+    path('profile/', ClientView.as_view(), name='profile'),
+    path('profile/change-password/', ClientView.as_view(), name='change_password'),
     path('auth/google/callback/', ClientView.as_view(), name='google_callback'),
+    # Server
+    path('api/auth/google/', GoogleLogin.as_view(), name='google_auth'),
 ]
