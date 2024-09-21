6f71af3f738416555cc6ecc8ada0a62769a670d2
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-09-21 21:15:57 +0200

    client: footer: integrate brand name from environment

--- src/client/src/apps/base/components/navigation/Footer.tsx
+++ src/client/src/apps/base/components/navigation/Footer.tsx
@@ -1,10 +1,12 @@
+import {BRAND_NAME} from "@/apps/base/constants/global.ts";
+
 const Footer = () => {
   const year = new Date().getFullYear();
 
   return (
     <footer className="absolute bottom-0 w-full border-t py-4 dark:bg-background">
       <div className="container mx-auto flex justify-center">
-        <p>Client &copy; {year}</p>
+        <p>{BRAND_NAME} &copy; {year}</p>
       </div>
     </footer>
   );
