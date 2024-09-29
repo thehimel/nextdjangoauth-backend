43a8d64b41a638cab0cd431896daae96ebd1d6c3
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-09-29 03:45:35 +0200

    server: use the frontend url for password resent email

--- src/apps/auth/adapters.py
+++ src/apps/auth/adapters.py
@@ -11,3 +11,15 @@ from django.conf import settings
 class AccountAdapter(DefaultAccountAdapter):
     def get_email_confirmation_url(self, request, emailconfirmation):
         return urljoin(settings.FRONTEND_URL, f'{verify_email_path}/{emailconfirmation.key}/')
+
+    def send_mail(self, template_prefix, email, context):
+        if 'password_reset' in template_prefix:
+            reset_password_path = 'auth/password/reset/confirm'  # Frontend reset password path
+            # Construct the URL with 'uid' and 'token'
+            reset_url = urljoin(
+                settings.FRONTEND_URL,
+                f'{reset_password_path}/{context["uid"]}/{context["token"]}/'
+            )
+            context['password_reset_url'] = reset_url
+
+        super().send_mail(template_prefix, email, context)
