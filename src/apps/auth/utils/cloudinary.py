2e8708ec3066e9bc7f146846f939c0c7aadcb468
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-11-01 02:33:22 +0100

    server: updated parse_cloudinary_url()

--- src/apps/auth/utils/cloudinary.py
+++ src/apps/auth/utils/cloudinary.py
@@ -1,10 +1,18 @@
 import re
 
-
 def parse_cloudinary_url(cloudinary_url: str) -> dict:
     """Example: CLOUDINARY_URL=cloudinary://<api_key>:<api_secret>@<cloud_name>"""
 
-    pattern = re.compile(r'cloudinary://(\w+):(\w+)@(\w+)')
+    # Regex to handle characters like hyphens, underscores, and others
+    pattern = re.compile(r'cloudinary://([^:]+):([^@]+)@([^/]+)')
     match = pattern.match(cloudinary_url)
 
-    return {'CLOUD_NAME': match.group(3), 'API_KEY': match.group(1), 'API_SECRET': match.group(2)}
+    # Check if match is None to avoid AttributeError
+    if not match:
+        raise ValueError("CLOUDINARY_URL format is incorrect")
+
+    return {
+        'CLOUD_NAME': match.group(3),
+        'API_KEY': match.group(1),
+        'API_SECRET': match.group(2)
+    }
